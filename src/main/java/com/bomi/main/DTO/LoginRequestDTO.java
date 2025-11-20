e1c325d0f4c01c206d79fddf70d19b1ae7c4de97
Kee0304 <kee0304@gmail.com>
2025-11-20 10:59:30 +0900

    phone nullable

--- src/main/java/com/bomi/main/DTO/LoginRequestDTO.java
+++ src/main/java/com/bomi/main/DTO/LoginRequestDTO.java
@@ -1,4 +1,9 @@
 package com.bomi.main.DTO;
 
 public record LoginRequestDTO(String email, String memberName, String memberPhone) {
+    public LoginRequestDTO {
+        if (memberPhone == null) {
+            memberPhone = "";
+        }
+    }
 }
